11ed0b5652d718dc468db90dbf2375749b37739b
brokenhoax <brokenhoax@outlook.com>
2024-11-16 00:55:47 -0600

    BLOG - 12 - new image viewer component

--- app/components/toggleImage/ToggleImage.tsx
+++ app/components/toggleImage/ToggleImage.tsx
@@ -3,15 +3,29 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import styles from "./ToggleImage.module.css";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/modal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 function ToggleImage({ params }) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [toggled, setToggled] = useState(false);
 
   function handleClick() {
     setToggled(!toggled);
+  }
+
+  function handleMaximize() {
+    onOpen();
+    console.log(isOpen);
   }
 
   if (!toggled) {
@@ -23,7 +37,7 @@ function ToggleImage({ params }) {
         {" "}
         <span className={`${styles.toggleStyles}`}>
           <FontAwesomeIcon icon={faCaretRight} fixedWidth></FontAwesomeIcon>
-          <div className={`${styles.toggleText}`}>Screenshot</div>
+          <div className={`${styles.toggleText}`}>Image</div>
         </span>
       </div>
     );
@@ -31,15 +45,16 @@ function ToggleImage({ params }) {
 
   if (toggled) {
     return (
-      <div
-        onClick={() => handleClick()}
-        className={`${styles.imageToggle} text-accent`}
-      >
-        <span className={`${styles.toggleStyles}`}>
+      <div className={`${styles.imageToggle} text-accent`}>
+        <span
+          onClick={() => handleClick()}
+          className={`${styles.toggleStyles}`}
+        >
           <FontAwesomeIcon icon={faCaretDown} fixedWidth></FontAwesomeIcon>
-          <div className={`${styles.toggleText}`}>Screenshot</div>
+          <div className={`${styles.toggleText}`}>Image</div>
         </span>
         <Image
+          onClick={() => handleMaximize()}
           className={`${styles.imageStyles}`}
           src={params.imagePath}
           width={params.width}
@@ -49,6 +64,44 @@ function ToggleImage({ params }) {
           alt={params.imageAltText}
           decoding="sync"
         ></Image>
+        <Modal
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+          size="5xl"
+          scrollBehavior="inside"
+          backdrop="blur"
+          classNames={{
+            backdrop: "bg-primary/50 backdrop-opacity-90",
+            base: "border-accent bg-primary dark:bg-primary text-accent",
+            header: "border-b-[1px] border-accent",
+            footer: "border-t-[1px] border-accent",
+            closeButton: "hover:bg-white/5 active:bg-white/10",
+          }}
+        >
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  {params.imageAltText}
+                </ModalHeader>
+                <ModalBody>
+                  <Image
+                    onClick={() => handleMaximize()}
+                    className={`${styles.imageStyles}`}
+                    src={params.imagePath}
+                    width={params.width}
+                    height={params.height}
+                    sizes={params.sizes}
+                    style={params.style}
+                    alt={params.imageAltText}
+                    decoding="sync"
+                  ></Image>
+                </ModalBody>
+                <ModalFooter></ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </div>
     );
   }
