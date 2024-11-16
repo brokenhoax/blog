ac550a80bd2f4bc5c9d3f480a25bad9cf0fea68b
brokenhoax <brokenhoax@outlook.com>
2024-11-16 00:54:41 -0600

    BLOG - 11 - new callout component

---
+++ app/components/callout/Callout.tsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import styles from "./Callout.module.css";
+
+type Callout = {
+  icon: IconProp;
+  text: number;
+};
+
+function Callout(callout: { icon: IconProp; text: string }) {
+  return (
+    <div className={`${styles.callout} bg-subtle`}>
+      <FontAwesomeIcon
+        icon={callout.icon}
+        className="text-white"
+        fixedWidth
+      ></FontAwesomeIcon>
+      <div className="pl-4 text-white">{callout.text}</div>
+    </div>
+  );
+}
+
+export default Callout;
