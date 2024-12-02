aacba95eb535e0c8014b67bb92b7df37db96c7dc
brokenhoax <brokenhoax@outlook.com>
2024-12-01 23:49:03 -0600

    BLOG - 28 - playing with fonts

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./global.css";
 import NavBar from "./components/navbar/NavBar";
 import Footer from "./components/footer/Footer";
-import { IBM_Plex_Mono } from "next/font/google";
+import { Bitter, IBM_Plex_Mono, Noto_Sans, PT_Serif } from "next/font/google";
 import { Providers } from "./providers";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -9,10 +9,30 @@ import "@fortawesome/free-brands-svg-icons";
 config.autoAddCss = false;
 
 // If loading a variable font, you don't need to specify the font weight
-const font = IBM_Plex_Mono({
+// const font = IBM_Plex_Mono({
+//   subsets: ["latin"],
+//   style: ["italic", "normal"],
+//   weight: ["100", "200", "300", "400", "500", "600", "700"],
+//   display: "swap",
+// });
+
+// const font = Noto_Sans({
+//   subsets: ["latin"],
+//   style: ["italic", "normal"],
+//   display: "swap",
+// });
+
+// const font = PT_Serif({
+//   subsets: ["latin"],
+//   style: ["italic", "normal"],
+//   weight: ["400", "700"],
+//   display: "swap",
+// });
+
+const font = Bitter({
   subsets: ["latin"],
   style: ["italic", "normal"],
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  weight: ["400", "700"],
   display: "swap",
 });
 
