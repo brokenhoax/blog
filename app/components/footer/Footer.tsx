1d583e388490141adac3a2db3749ded82f82a57e
Andrew Kraus <brokenhoax@outlook.com>
2024-12-23 16:27:45 -0600

    BLOG - 43 - updated footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,8 +1,28 @@
 "use client";
+import Image from "next/image";
 import styles from "./Footer.module.css";
 
 function Footer() {
-  return <div className={`${styles.footer}`}></div>;
+  const logo = {
+    id: "1",
+    imagePath: "/images/kraus-cloud-logo.svg",
+    imageAltText: "Kraus Cloud logo",
+    width: 100,
+    height: 0,
+    style: { width: "100%", height: "100%" },
+  };
+  return (
+    <div className={`${styles.footer}`}>
+      <Image
+        src={logo.imagePath}
+        width="10"
+        height={logo.height}
+        alt={logo.imageAltText}
+        decoding="sync"
+        className={`${styles.logo}`}
+      ></Image>
+    </div>
+  );
 }
 
 export default Footer;
