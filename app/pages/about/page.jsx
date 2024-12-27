66a32eb33a457cc5e8b4c446dee4a8881ccb8da4
Andrew Kraus <brokenhoax@outlook.com>
2024-12-26 19:57:19 -0600

    BLOG - 46 - about page updates

--- app/pages/about/page.jsx
+++ app/pages/about/page.jsx
@@ -8,20 +8,13 @@ function About() {
   return (
     <section className="section">
       <h1>About</h1>
-      <div className={`${styles.socialLinksWrapper}`}>
-        <FontAwesomeIcon
-          icon={faXTwitter}
-          className={`${styles.socialLinks}`}
-        />
-        <FontAwesomeIcon
-          icon={faLinkedin}
-          className={`${styles.socialLinks}`}
-        />
-      </div>
-      <div className="border-b border-accent"></div>
       {/* Headline */}
-      <div className="headlineWrapper">
-        <p className="headline">
+      <div className="block border-b border-accent"></div>
+      <div>
+        <h2 className={`${styles.aboutTheWriter} text-accent text-regular`}>
+          Kraus Cloud
+        </h2>
+        <p className="italic">
           Kraus Cloud is an educational blog designed for readers. It will
           include posts on web development, web design, user experience, home
           labs, cyberscurity, networking, linux, and more. Our content is
@@ -32,10 +25,10 @@ function About() {
           content you were hoping to find, let me know. Thanks for stopping by
           and enjoy the site!
         </p>
-      </div>
-      <div className="block border-b border-accent"></div>
-      <div className={`${styles.aboutTheWriter}`}>
-        <h2 className="text-accent">About the Writer</h2>
+        <div className="faintDivider block border-b border-accent"></div>
+        <h2 className={`${styles.aboutTheWriter} text-accent text-regular`}>
+          The Writer
+        </h2>
         <p>
           My name is Andrew Kraus and I enjoy all things tech and design.
           Eighteen years into my career and I've worked in tech as a reseller, a
@@ -53,35 +46,9 @@ function About() {
           of the web to share what I've learned. Kraus Cloud is self-hosted and,
           by following the free courses on this site, you, too, can build your
           own home lab and website. Kraus Cloud was built using Next.js,
-          TailwindCSS, Node, pfSense, Proxmox, Red Hat to name a few.
+          TailwindCSS, Node, pfSense, Proxmox, Red Hat to name a few. Oh, yeah,
+          and a lot of coffee and late nights.
         </p>
-        <p>Oh, yeah, and a lot of coffee and late nights.</p>
-        <FontAwesomeIcon icon={faMugHot} className={`${styles.socialLinks}`} />
-      </div>
-      {/* Contact Details */}
-      <div className="">
-        <div className="flex flex-col">
-          <div className="space-x-2">
-            {/* LinkedIn */}
-            <a
-              href="https://www.linkedin.com/in/andrewkraus/"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:text-accent text-gray-700"
-            >
-              <i className="fab fa-linkedin"></i>
-            </a>
-            {/* GitHub */}
-            <a
-              href="https://github.com/brokenhoax/"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:text-accent text-gray-700"
-            >
-              <i className="fab fa-github"></i>
-            </a>
-          </div>
-        </div>
       </div>
     </section>
   );
