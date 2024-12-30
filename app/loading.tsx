f59b824c9212b0f2d006393bc320cb511a47d4fa
Andrew <brokenhoax@outlook.com>
2024-12-29 21:07:46 -0600

    BLOG - 80 - loading state mvp

---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import styles from "./Loading.module.css";
+
+function Loading() {
+
+  return (
+    <div className={`${styles.loadingWrapper}`}>
+      <p>Loading...</p>
+    </div>
+  );
+}
+
+export default Loading;
