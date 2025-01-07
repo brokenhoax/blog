369de78e456329ada8002a741c7a0a6de81852f2
brokenhoax <brokenhoax@outlook.com>
2025-01-07 10:39:42 -0600

    BLOG - 135 - table of contents updates

--- app/components/toc/TableOfContents.tsx
+++ app/components/toc/TableOfContents.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import Link from "next/link";
 import styles from "./TableOfContents.module.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
