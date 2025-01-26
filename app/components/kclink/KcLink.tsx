77edf214d3eab2530b013ba6d8b310af4312c28b
brokenhoax <brokenhoax@outlook.com>
2025-01-25 22:54:25 -0600

    BLOG - 156 - kclink updates

--- app/components/kclink/KcLink.tsx
+++ app/components/kclink/KcLink.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import React, { ReactNode, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAmazon } from "@fortawesome/free-brands-svg-icons";
+import KcButton from "../kcbutton/KcButton";
 import {
-  faLink,
   faCaretDown,
-  faCaretUp,
+  faCaretLeft,
   faShoppingCart,
 } from "@fortawesome/free-solid-svg-icons";
 import styles from "../kclink/KcLink.module.css";
@@ -14,7 +12,7 @@ import styles from "../kclink/KcLink.module.css";
 type KcProps = {
   webLink: string;
   productDescription: string;
-  hideAmazon: boolean;
+  hideLink: boolean;
   elementId: string;
   children: ReactNode;
 };
@@ -22,59 +20,44 @@ type KcProps = {
 const KcLink = ({
   webLink,
   productDescription,
-  hideAmazon,
+  hideLink,
   elementId,
   children,
 }: KcProps) => {
   const [toggled, setToggled] = useState(true);
-  let icon = faLink;
-  if (!hideAmazon) {
-    icon = faAmazon;
-  }
 
   function handleClick() {
     setToggled(!toggled);
     console.log(toggled);
   }
 
+  let AmazonLinkButton = <></>;
+  if (!hideLink) {
+    AmazonLinkButton = (
+      <KcButton icon={faShoppingCart} type="link" url={webLink}></KcButton>
+    );
+  }
   if (!toggled) {
     return (
       <div className={`${styles.kcLinkWrapper}`} id={elementId}>
-        <a
-          href={webLink}
-          className={`${styles.kcLinkCollapsed} bg-subtle`}
-          target="_blank"
-        >
-          <div className={`${styles.kcLinkDescription} hover:text-accent`}>
-            <FontAwesomeIcon
-              icon={icon}
-              fixedWidth={true}
-              size="lg"
-              className={`${styles.kcLinkIcon}`}
-            ></FontAwesomeIcon>
+        <div className={`${styles.kcLinkCollapsed} bg-subtle`}>
+          <div className={`${styles.kcLinkDescription} text-accent`}>
             <div className={`${styles.productDescription} text-accent`}>
               {productDescription}
             </div>
           </div>
-          <button className={`${styles.kcLinkBtn} bg-accent hover:text-white`}>
-            <FontAwesomeIcon
-              icon={faShoppingCart}
-              fixedWidth={true}
-              size="sm"
-            ></FontAwesomeIcon>
-          </button>
-        </a>
-        {/* <button
-          onClick={() => handleClick()}
-          className={`${styles.teaser} bg-accent`}
-        >
-          <FontAwesomeIcon
-            icon={faCaretUp}
-            fixedWidth={true}
-            size="xs"
-            className={`${styles.teaserLinkIcon} text-white`}
-          ></FontAwesomeIcon>
-        </button> */}
+          <div className={`${styles.kcLinkButtons}`}>
+            {/* Amazon Associate Link */}
+            {AmazonLinkButton}
+            {/* Open Text Box */}
+            <KcButton
+              icon={faCaretLeft}
+              isToggled={toggled}
+              onToggle={handleClick}
+              type="toggle"
+            ></KcButton>
+          </div>
+        </div>
       </div>
     );
   }
@@ -82,31 +65,24 @@ const KcLink = ({
   if (toggled) {
     return (
       <div className={`${styles.kcLinkWrapper}`} id={elementId}>
-        <a
-          href={webLink}
-          className={`${styles.kcLinkExpanded} bg-subtle hover:text-accent`}
-          target="_blank"
-        >
-          <FontAwesomeIcon
-            icon={icon}
-            fixedWidth={true}
-            size="lg"
-            className={`${styles.kcLinkIcon}`}
-          ></FontAwesomeIcon>
-          <span className="text-accent">{productDescription}</span>
-        </a>
-        <button
-          onClick={() => handleClick()}
-          className={`${styles.teaser} bg-accent`}
-        >
-          {" "}
-          <FontAwesomeIcon
-            icon={faCaretDown}
-            fixedWidth={true}
-            size="xs"
-            className={`${styles.teaserLinkIcon}`}
-          ></FontAwesomeIcon>
-        </button>
+        <div className={`${styles.kcLinkCollapsed} bg-subtle`}>
+          <div className={`${styles.kcLinkDescription} text-accent`}>
+            <div className={`${styles.productDescription} text-accent`}>
+              {productDescription}
+            </div>
+          </div>
+          <div className={`${styles.kcLinkButtons}`}>
+            {/* Amazon Associate Link */}
+            {AmazonLinkButton}
+            {/* Close Text Box */}
+            <KcButton
+              icon={faCaretDown}
+              isToggled={toggled}
+              onToggle={handleClick}
+              type="toggle"
+            ></KcButton>
+          </div>
+        </div>
         <div className={`${styles.teaserBody} text-tertiary`}>{children}</div>
       </div>
     );
