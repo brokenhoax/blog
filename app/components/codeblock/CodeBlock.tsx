dfe43bfffb2360e6020cf4562b720827b553d698
Andrew <brokenhoax@outlook.com>
2025-11-20 12:37:30 -0600

    BLOG - 235 - update to caniuse

--- app/components/codeblock/CodeBlock.tsx
+++ app/components/codeblock/CodeBlock.tsx
@@ -1,9 +1,17 @@
+"use client";
+import { useEffect, useState } from "react";
 import hljs from "highlight.js";
 import DOMPurify from "isomorphic-dompurify";
 
 function CodeBlock({ props, type }: { props: string; type: string }) {
+  const [isClient, setIsClient] = useState(false); // Track if we're on the client
+
+  useEffect(() => {
+    setIsClient(true); // Set to true after component mounts in the browser
+  }, []);
+
   // Sanitize our inputs for better security
-  const sanitizedCode = DOMPurify.sanitize(props);
+  const sanitizedCode = isClient ? DOMPurify.sanitize(props) : props;
   // Do not sanitize code
   const unsanitizedCode = props;
   // Pass in either sanitized or unsanitized code
